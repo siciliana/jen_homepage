cd3ceb38036e43025c0dea2dd9040246653e9475
siciliana <siciliana@gmail.com>
2013-12-06 17:57:01 -0500

    cleaning up formatting

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,14 +1,23 @@
 $(document).ready(function(){
 	$('.jen').on('click', function() {
 		$('.jen').toggle('slow')
-	})
+	});
 
 	$('.social_header').hide(); 
-
 	$('#follow').on('click', function() {
 		$('.teal').css("height", "100px");
 					$('.social_header').toggle('slow')
 	});  
+
+	$('.cfh').hide();
+	$('#cfh').on('click', function(){
+		$('.cfh').toggle(); 
+	})
+
+	$('.btc').hide();
+	$('#btc').on('click', function(){
+		$('.btc').toggle(); 
+	})
 });
 
 	// $('.social').toggle( "bounce", { times: 3 }, "slow" )
