04aeb9b908bfcff504044ec04f7eab8ef632fca7
siciliana <siciliana@gmail.com>
2013-12-06 00:18:34 -0500

    added toggling and social media icons to header

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
-	console.log("test")
-	$('button').click(function(){
-		('p').toggle( "slow" ); 
+	$('.jen').on('click', function() {
+		$('.jen').toggle('slow')
 	})
-})
+
+	$('.social_header').hide(); 
+	$('#follow').on('click', function() {
+		$('.teal').css("height", "100px")
+		$('.social_header').toggle('slow')
+	})
+});
