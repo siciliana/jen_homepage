bf54746fb4a71289f7e7e2be3b16b753e5e5d9c7
siciliana <siciliana@gmail.com>
2013-12-07 18:52:52 -0500

    updated js and album display

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -3,21 +3,21 @@ $(document).ready(function(){
 		$('.jen').toggle('slow')
 	});
 
-	$('.social_header').hide(); 
-	$('#follow').on('click', function() {
-		$('.teal').css("height", "100px");
-					$('.social_header').toggle('slow')
+	// $('.cfh').hide();
+	// $('#cfh').on('click', function(){
+	// 	$('.cfh').toggle(); 
+	// })
+
+	// $('.btc').hide();
+	// $('#btc').on('click', function(){
+	// 	$('.btc').toggle(); 
+	// })
+
+	// // $('.social_header').hide(); 
+	// $('#follow').on('click', function() {
+	// 	$('.teal').css("height", "100px");
+	// 	$('.social_header').show('slow');
+
 	});  
-
-	$('.cfh').hide();
-	$('#cfh').on('click', function(){
-		$('.cfh').toggle(); 
-	})
-
-	$('.btc').hide();
-	$('#btc').on('click', function(){
-		$('.btc').toggle(); 
-	})
 });
 
-	// $('.social').toggle( "bounce", { times: 3 }, "slow" )
